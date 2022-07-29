e52f028d25db8ad85f0beb63081d7d157eb5af27
Julien <julien7henry@gmail.com>
2022-07-29 10:21:21 +0200

    states of submitted form are handled, clear if the message is send, keep values if there's a server error

--- client/src/components/ContactForm.jsx
+++ client/src/components/ContactForm.jsx
@@ -1,91 +1,108 @@
-import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { yupResolver } from '@hookform/resolvers/yup';
 import { contactForm } from "../validationSchema";
-import { submitForm } from "../api/fetch";
 import { ExclamationIcon } from "@heroicons/react/solid";
 import FormLoader from "./FormLoader";
 import FormError from "./FormError";
 import FormSuccess from "./FormSuccess";
+import { useState } from "react";
+import { useRef } from "react";
 
 
 const ContactForm = () => {
-    const { register, getValues, handleSubmit, formState: { errors } } = useForm({
+    const { register, getValues, handleSubmit, reset, formState: { isSubmitting, isSubmitSuccessful, errors } } = useForm({
         mode: 'onSubmit',
         resolver: yupResolver(contactForm),
     });
-
-
+    const [error, setErrorState] = useState(false)
+    const data = getValues();
+    const ref = useRef(null)
+    const form = ref.current;
 
     const onSubmit = async () => {
         try {
-            // setIsLoading(true);
-            const data = getValues();
-            await submitForm('http://localhost:8080/api/contact', data);
+            const options = {
+                method: "POST",
+                headers: {
+                    "Content-type": "application/json; charset=UTF-8",
+                },
+                body: JSON.stringify({ ...data }),
+            };
+            const res = await fetch('http://localhost:8080/api/contact', options);
+            const resData = await res.json();
+            return resData
         } catch (error) {
-            console.log(error);
+            setErrorState(true);
+            throw new Error('Failed to contact server')
         }
     }
 
+    const handleReset = () => {
+        if (error) return setErrorState(false);
+        else return reset(form)
+    }
 
     return (
         <>
-            {/* {isLoading ? <FormLoader /> :
-                isError ?
+            {isSubmitting ? <FormLoader /> :
+                error ?
                     <div className="w-full flex flex-col items-center gap-y-2">
                         <FormError />
-                        <p className='text-lg text-secondary'>Retry</p>
+                        <p className='text-lg text-secondary cursor-pointer hover:underline' onClick={() => handleReset()}>Retry</p>
                     </div>
                     :
-                    isSuccess ?
+                    isSubmitSuccessful ?
                         <div className="w-full flex flex-col items-center gap-y-2">
                             <FormSuccess />
-                            <p className='text-base text-secondary hover:underline'>Send a new message</p>
+                            <p className='text-base text-secondary cursor-pointer hover:underline' onClick={() => handleReset()} >Send a new message</p>
                         </div>
-                        : */}
-            <form
-                className="w-full p-8 flex flex-col items-center gap-y-8 rounded-[20px] bg-tertiary md:max-w-lg md:rounded-b-none"
-                onSubmit={handleSubmit(onSubmit)}>
+                        :
+                        <form
+                            id="form"
+                            ref={ref}
+                            className="w-full p-8 flex flex-col items-center gap-y-8 rounded-[20px] bg-tertiary md:max-w-lg md:rounded-b-none"
+                            onSubmit={handleSubmit(onSubmit)}>
 
-                <div className="w-full flex flex-col items-start gap-y-2">
-                    <input type="text" name="name" placeholder="Name" className="w-full h-14 p-5 rounded-[20px] text-xl outline-quaternary outline-2 focus:outline-double" {...register("name")} required />
-                    {errors.name &&
-                        <div className="w-full flex items-center gap-x-2 text-yellow-400">
-                            <ExclamationIcon className="w-5 h-5" />
-                            <p>{errors.name.message}</p>
-                        </div>
-                    }
-                </div>
-                <div className="w-full flex flex-col items-start gap-y-2">
-                    <input type="email" name="email" placeholder="Email address" className="w-full h-14 p-5 rounded-[20px] text-xl outline-quaternary outline-2 focus:outline-double" {...register("email")} required />
-                    {errors.email &&
-                        <div className="w-full flex items-center gap-x-2 text-yellow-400">
-                            <ExclamationIcon className="w-5 h-5" />
-                            <p>{errors.email.message}</p>
-                        </div>
-                    }
-                </div>
-                <div className="w-full flex flex-col items-start gap-y-2">
-                    <input type="text" name="subject" placeholder="Subject" className="w-full h-14 p-5 rounded-[20px] text-xl outline-quaternary outline-2 focus:outline-double" {...register("subject")} required />
-                    {errors.subject &&
-                        <div className="w-full flex items-center gap-x-2 text-yellow-400">
-                            <ExclamationIcon className="w-5 h-5" />
-                            <p>{errors.subject.message}</p>
-                        </div>
-                    }
-                </div>
-                <div className="w-full flex flex-col items-start gap-y-2">
-                    <textarea name="message" placeholder="Write you message..." minLength='2' className="w-full min-h-[300px] p-5 rounded-[20px] text-xl outline-quaternary outline-2 focus:outline-double" {...register("message")} required />
+                            <div className="w-full flex flex-col items-start gap-y-2">
+                                <input type="text" name="name" placeholder="Name" className="w-full h-14 p-5 rounded-[20px] text-xl outline-quaternary outline-2 focus:outline-double" {...register("name")} required />
+                                {errors.name &&
+                                    <div className="w-full flex items-center gap-x-2 text-yellow-400">
+                                        <ExclamationIcon className="w-5 h-5" />
+                                        <p>{errors.name.message}</p>
+                                    </div>
+                                }
+                            </div>
+                            <div className="w-full flex flex-col items-start gap-y-2">
+                                <input type="email" name="email" placeholder="Email address" className="w-full h-14 p-5 rounded-[20px] text-xl outline-quaternary outline-2 focus:outline-double" {...register("email")} required />
+                                {errors.email &&
+                                    <div className="w-full flex items-center gap-x-2 text-yellow-400">
+                                        <ExclamationIcon className="w-5 h-5" />
+                                        <p>{errors.email.message}</p>
+                                    </div>
+                                }
+                            </div>
+                            <div className="w-full flex flex-col items-start gap-y-2">
+                                <input type="text" name="subject" placeholder="Subject" className="w-full h-14 p-5 rounded-[20px] text-xl outline-quaternary outline-2 focus:outline-double" {...register("subject")} required />
+                                {errors.subject &&
+                                    <div className="w-full flex items-center gap-x-2 text-yellow-400">
+                                        <ExclamationIcon className="w-5 h-5" />
+                                        <p>{errors.subject.message}</p>
+                                    </div>
+                                }
+                            </div>
+                            <div className="w-full flex flex-col items-start gap-y-2">
+                                <textarea name="message" placeholder="Write you message..." minLength='2' className="w-full min-h-[300px] p-5 rounded-[20px] text-xl outline-quaternary outline-2 focus:outline-double" {...register("message")} required />
 
-                    {errors.message &&
-                        <div className="w-full flex items-center gap-x-2 text-yellow-400">
-                            <ExclamationIcon className="w-5 h-5" />
-                            <p>{errors.message.message}</p>
-                        </div>
-                    }
-                </div>
-                <input type="submit" value="Send message" className='w-fit h-auto px-4 py-2 rounded-3xl text-[22px] bg-secondary text-primary font-bold cursor-pointer hover:bg-secondaryLighten disabled:bg-quaternary' />
-            </form>
+                                {errors.message &&
+                                    <div className="w-full flex items-center gap-x-2 text-yellow-400">
+                                        <ExclamationIcon className="w-5 h-5" />
+                                        <p>{errors.message.message}</p>
+                                    </div>
+                                }
+                            </div>
+                            <input type="submit" value="Send message" className='w-fit h-auto px-4 py-2 rounded-3xl text-[22px] bg-secondary text-primary font-bold cursor-pointer hover:bg-secondaryLighten disabled:bg-quaternary' />
+                        </form>
+            }
         </>
     );
 };
--- client/src/components/FormLoader.jsx
+++ client/src/components/FormLoader.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const FormLoader = () => {
     return (
         <div className='w-full h-full flex items-center justify-center z-50'>
-            <span className='w-12 h-12 inline-block border-quaternary border-t-secondary rounded-[50%] animate-clockMinuteHand ' />
+            <span className='w-16 h-16 border-8 border-secondary border-t-quaternary border-w rounded-[50%] animate-clockMinuteHand ' />
         </div>
     );
 };
