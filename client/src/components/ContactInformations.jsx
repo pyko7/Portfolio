cb25dfcd30080e46dd1072b4dcbe0fe951834724
Julien <julien7henry@gmail.com>
2022-08-29 14:41:13 +0200

    removing of heroicons library

--- client/src/components/ContactInformations.jsx
+++ client/src/components/ContactInformations.jsx
@@ -3,36 +3,36 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
-import { PhoneIcon } from '@heroicons/react/solid'
+import { faPhone } from '@fortawesome/free-solid-svg-icons';
 
 const ContactInformations = () => {
     return (
         <ul className='w-full flex flex-col gap-y-10 items-start text-secondary text-justify'>
-            <li className='w-fit flex items-center gap-x-2 text-lg' aria-label="Email me">
-                <FontAwesomeIcon icon={faEnvelope} aria-hidden='false' aria-label='Email' className='w-7 h-7 hover:drop-shadow-[0_2px_4px_rgba(220,215,201,0.64)]' />
-                <a href="mailto:julien.f.henry@gmail.com" target="_blank" rel='noreferrer' className='focus-visible:outline-items'>
+            <li>
+                <a href="mailto:julien.f.henry@gmail.com" target="_blank" rel='noreferrer' aria-label="Email me" className='w-fit flex items-center gap-x-2 text-lg focus-visible:outline-items'>
+                    <FontAwesomeIcon icon={faEnvelope} className='w-7 h-7 hover:drop-shadow-[0_2px_4px_rgba(220,215,201,0.64)]' />
                     julien.f.henry@gmail.com
                 </a>
             </li>
-            <li className='w-fit flex items-center gap-x-2 text-lg'>
-                <FontAwesomeIcon icon={faLinkedin} aria-hidden='false' aria-label="LinkedIn" className='w-7 h-7 hover:drop-shadow-[0_2px_4px_rgba(220,215,201,0.64)]' />
-                <a href="https://www.linkedin.com/in/julienhenry9671" target="_blank" rel='noreferrer' className='focus-visible:outline-items'>
+            <li>
+                <a href="https://www.linkedin.com/in/julienhenry9671" target="_blank" rel='noreferrer' className='w-fit flex items-center gap-x-2 text-lg focus-visible:outline-items'>
+                    <FontAwesomeIcon icon={faLinkedin} aria-hidden='false' aria-label="LinkedIn" className='w-7 h-7 hover:drop-shadow-[0_2px_4px_rgba(220,215,201,0.64)]' />
                     julienhenry9671
                 </a>
             </li>
-            <li className='w-fit flex items-center gap-x-2 text-lg'>
-                <PhoneIcon aria-hidden='false' aria-label="Phone number" className='w-7 h-7 hover:drop-shadow-[0_2px_4px_rgba(220,215,201,0.64)]' />
-                <span tabIndex="0" className='focus-visible:outline-items'>
+            <li className='w-fit flex items-center gap-x-2 text-lg focus-visible:outline-items'>
+                <FontAwesomeIcon icon={faPhone} aria-hidden='false' aria-label="Phone number" className='w-6 h-6 hover:drop-shadow-[0_2px_4px_rgba(220,215,201,0.64)]' />
+                <span tabIndex="0" className='w-fit flex items-center gap-x-2 text-lg focus-visible:outline-items'>
                     +33 (0)6.26.05.10.58
                 </span>
             </li>
-            <li className='w-fit flex items-center gap-x-2 text-lg'>
-                <a href="https://github.com/pyko7" target="_blank" rel='noreferrer' className='w-fit flex items-center gap-x-2 text-lg hover:drop-shadow-[0_2px_4px_rgba(220,215,201,0.64)]'>
-                    <FontAwesomeIcon icon={faGithub} aria-hidden='false' aria-label="Github" className='w-7 h-7 focus-visible:outline-items' />
-                    Pyko7
-                </a>
-            </li>
-        </ul>
+            <li>
+                <a href="https://github.com/pyko7" target="_blank" rel='noreferrer' className='w-fit flex items-center gap-x-2 text-lg focus-visible:outline-items'>
+                <FontAwesomeIcon icon={faGithub} aria-hidden='false' aria-label="Github" className='w-6 h-6 hover:drop-shadow-[0_2px_4px_rgba(220,215,201,0.64)]' />
+                Pyko7
+            </a>
+        </li>
+        </ul >
     );
 };
 
--- client/src/components/ProjectImageOverlay.jsx
+++ client/src/components/ProjectImageOverlay.jsx
@@ -2,13 +2,12 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
-import { ExternalLinkIcon } from '@heroicons/react/outline';
 
 
-const ProjectImageOverlay = () => {
+const ProjectImageOverlay = ({reverse}) => {
     return (
-        <div className='absolute top-0 left-0 w-full h-full flex justify-center items-center gap-x-8 bg-black/[.5] z-10' >
-            <a href='https://github.com/pyko7/P7-Groupomania' className="w-8 h-8 flex items-center hover:drop-shadow-[0_2px_4px_rgba(220,215,201,0.64)]">
+        <div className='absolute top-0 left-0 w-full h-full flex justify-center items-center gap-x-8 bg-black/[.75] backdrop-blur-sm z-10' >
+            <a href={reverse ? 'https://github.com/pyko7/Mooviz' : 'https://github.com/pyko7/P7-Groupomania'} className="w-8 h-8 flex items-center hover:drop-shadow-[0_2px_4px_rgba(220,215,201,0.64)]">
                 <FontAwesomeIcon icon={faGithub} aria-label="Github" className="h-full w-full" />
             </a>
             <a href='#' className="w-8 h-8 flex items-center hover:drop-shadow-[0_2px_4px_rgba(220,215,201,0.64)]">
