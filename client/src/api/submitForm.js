d97819f8732f1de1ed904a21050e9ef3be1c9dd1
Julien <julien7henry@gmail.com>
2022-09-02 07:10:32 +0200

    update of submitForm API URL

--- client/src/api/submitForm.js
+++ client/src/api/submitForm.js
@@ -7,7 +7,7 @@ export const postData = async (payload) => {
       },
       body: JSON.stringify({ ...payload }),
     };
-    const res = await fetch("http://localhost:3000/api/contact", options);
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/contact`, options);
     const data = await res.json();
     return data;
   } catch (error) {
