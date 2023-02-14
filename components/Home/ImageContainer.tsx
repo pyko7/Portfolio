6316d5f51bb63590374a7e099f9f600fdb61be7d
Julien Henry <julienhenry967@gmail.com>
2023-02-14 14:30:51 +0100

    refactor: add priority to image

--- components/Home/ImageContainer.tsx
+++ components/Home/ImageContainer.tsx
@@ -7,7 +7,7 @@ const ImageContainer = () => {
       className="relative w-96 h-96 lg:w-80 lg:h-80 md:w-64 md:h-64 sm:opacity-0"
       id="photo"
     >
-      <Image src={photo} style={{ objectFit: "contain" }} alt="julien henry" />
+      <Image src={photo} style={{ objectFit: "contain" }} priority alt="julien henry" />
     </div>
   );
 };
