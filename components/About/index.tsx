7c399210ee966c80408daa4e3b23133b28e06058
julien henry <julienhenry967@gmail.com>
2023-04-06 17:10:00 +0200

    fix: update a tag to link

--- components/About/index.tsx
+++ components/About/index.tsx
@@ -7,9 +7,7 @@ const Informations = () => {
         <h2 className="text-6xl animate-translateToRight sm:text-4xl">
           Julien
         </h2>
-        <h2 className="text-6xl animate-translateToLeft sm:text-4xl">
-          Henry
-        </h2>
+        <h2 className="text-6xl animate-translateToLeft sm:text-4xl">Henry</h2>
       </div>
 
       <div className="w-full flex justify-center text-3xl opacity-0 animate-fastFadeIn sm:text-lg">
@@ -23,12 +21,12 @@ const Informations = () => {
         >
           Contact
         </Link>
-        <a
+        <Link
           href="/work"
           className="hover:underline after:content-['>'] after:inline-block after:pl-[4px] after:cursor-default hover:after:no-underline"
         >
           Work
-        </a>
+        </Link>
       </div>
     </div>
   );
