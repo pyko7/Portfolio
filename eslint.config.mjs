840208d8df2f9160de6a7ada4a3b17d4c28d054f
julien henry <julienhenry967@gmail.com>
2025-06-05 18:40:22 +0200

    chore: setup linter

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,19 @@
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc'
 
 const compat = new FlatCompat({
   // import.meta.dirname is available after Node.js v20.11.0
-  baseDirectory: import.meta.dirname,
-});
+  baseDirectory: import.meta.dirname
+})
 
 const eslintConfig = [
   ...compat.config({
-    extends: ["next/core-web-vitals", "next/typescript"],
-  }),
-];
+    extends: ['next/core-web-vitals', 'next/typescript', 'prettier'],
+    rules: {
+      semi: ['error', 'never'],
+      quotes: ['error', 'single', { avoidEscape: true }],
+      'no-useless-return': 'error'
+    }
+  })
+]
 
-export default eslintConfig;
+export default eslintConfig
