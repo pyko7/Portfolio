20a33ad9212d2857b18794d09f32c5ba34d8b4d7
julien henry <julienhenry967@gmail.com>
2025-06-06 18:21:14 +0200

    chore: refacto navbar

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,21 +1,11 @@
-import { PATHNAMES } from '@/utils/_utils'
-import Link from 'next/link'
+import { generateRandomGuid, PATHNAMES } from '@/utils/_utils'
+import NavbarItem from './NavbarItem'
 
 const Navbar = () => (
   <nav className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full flex justify-center">
     <ul className="hidden w-full items-center justify-center gap-10 list-none sm:flex md:gap-20">
       {PATHNAMES.map((pathname) => (
-        <li
-          className="text-sm text-main-font-color no-underline uppercase leading-6 tracking-widest xl:text-lg hover:font-semibold"
-          key={pathname}
-        >
-          <Link
-            className="block h-full"
-            href={pathname === 'home' ? '/' : pathname}
-          >
-            {pathname}
-          </Link>
-        </li>
+        <NavbarItem key={generateRandomGuid()} pathname={pathname} />
       ))}
     </ul>
   </nav>
